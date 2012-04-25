ef3af681953a26885f98626b1708325734f80e6f
James Hagerman <james@42entertainment.com>
2012-04-25 12:24:50 -0700

    first commit to template

---
+++ javascripts/debug.js
@@ -0,0 +1,100 @@
+// This debug library lets you use the debug(msg) debugLn(msg) and debugClear() commands when ever you want.
+// 
+// It officially depends on Agility to manage the debug view but it gracefully falls back as long as jQuery
+// is available
+
+var debugStyle = "z-index: 99;" + 
+"position: absolute;" +
+"top: 0px;" + 
+"right: 0px;" +
+"min-width: 300px;" +
+//"height: 200px;" +
+//"display: none;" +
+"background-color: #070;" +
+"opacity: 0.9;" +
+"font-size: 0.9em;" +
+"overflow: scroll;" +
+"font-family: 'Courier New', Courier, monospace;";
+var _onPage;
+var _version;
+try {
+	var Debug = {
+		debugMVC: $$({
+			model: {debug_text: 'Debug:'},
+			view: {
+				format: "<div data-bind='debug_text' data-type='html'/>",
+				style: "& {" + debugStyle + "}"
+			}
+		}),
+		_onPage: false,
+		init: function() {
+			$$.document.append(this.debugMVC);
+			this.onPage = true;
+			_version = "agility";
+		},
+		log: function(newText) {
+			if (!this._onPage) {this.init()}
+			var existingDebug = this.debugMVC.model.get("debug_text");
+			this.debugMVC.model.set({
+				debug_text: existingDebug + newText
+			});
+		},
+		logLn: function(newText) {
+			if (!this._onPage) {this.init()}
+			var existingDebug = this.debugMVC.model.get("debug_text");
+			this.debugMVC.model.set({
+				debug_text: existingDebug + "<br />" + newText
+			});
+		},
+		clear: function() {
+			if (!this._onPage) {this.init()}
+			this.debugMVC.model.set({
+				debug_text: "Debug:"
+			}, {reset:true});
+		}
+	};
+}
+catch(err) {
+	console.log("");
+	console.log("There was an error setting up the Agility debugging layer: " + err);
+	Debug = false;
+	_onPage = false;
+}
+
+// Simple debug methods
+function initDebug() {
+	_onPage = true;
+	_version = "jquery";
+	$('body').append('<div class="debug" style="' + debugStyle + '">Debug:</div>');
+}
+
+function debug(toSpit) {
+	if ( Debug !== false) {
+		Debug.log(toSpit);
+	} else {
+		if (!_onPage) {initDebug()}
+		$('.debug').append("  |" + toSpit);
+	}
+}
+function debugLn(toSpit) {
+	if ( Debug !== false) {
+		Debug.logLn(toSpit);
+	} else {
+		if (!_onPage) {initDebug()}
+		$('.debug').append("<br />" + toSpit );
+	}
+}
+function debugClear() {
+	if ( Debug !== false) {
+		Debug.clear();
+	} else {
+		if (!_onPage) {initDebug()}
+		$('.debug').html("<span>Debug:</span><br />");
+	}
+}
+
+
+// Override native console.log. Probably not the best idea...
+console.log = function (msg) {
+	debugLn(msg);
+}
---
+++ javascripts/helpers.js
@@ -0,0 +1,56 @@
+// Useful helpers:
+function copyCssStyle(elementTo, elementFrom, stylename) {
+	//debugLn("element to pull CSS from: style: " + stylename + " element: " + $(elementFrom).attr('class') + " value: " + $(elementFrom).css(stylename));
+	//debugLn("element to send CSS too: style: " + stylename + " element: " + $(elementTo).attr('class') + " value: " + $(elementTo).css(stylename));
+	$(elementTo).css(stylename, $(elementFrom).css(stylename));
+	//debugLn("element to pull CSS from: style: " + stylename + " element: " + $(elementFrom).attr('class') + " value: " + $(elementFrom).css(stylename));
+	//debugLn("element to send CSS too: style: " + stylename + " element: " + $(elementTo).attr('class') + " value: " + $(elementTo).css(stylename));
+}
+
+function randomBetween(start, end){
+   return Math.floor(Math.random() * (end - start + 1) + start);
+}
+
+function isTouchDevice() {
+	var el = document.createElement('div');
+	el.setAttribute('id',"touchtesttoremove");
+	el.setAttribute('ongesturestart', 'return;');
+	if(typeof el.ongesturestart == "function"){
+		$('#touchtesttoremove').remove();
+		return true;
+	} else {
+		$('#touchtesttoremove').remove();
+		return false;
+	}
+}
+
+// $(function() {
+//   window.keydown = {};
+//   
+//   function keyName(event) {
+//     return jQuery.hotkeys.specialKeys[event.which] ||
+//       String.fromCharCode(event.which).toLowerCase();
+//   }
+//   
+//   $(document).bind("keydown", function(event) {
+//     keydown[keyName(event)] = true;
+//     event.preventDefault();
+//   });
+//   
+//   $(document).bind("keyup", function(event) {
+//     keydown[keyName(event)] = false;
+//     event.preventDefault();
+//   });
+// });
+
+// Debug tools:
+function dumpObject(myObj) {
+	//debugClear();
+	console.log("");
+	for (myKey in myObj){
+		console.log("myObj["+myKey +"] = "+myObj[myKey]);
+	}
+}
+function stringify(myObj) {
+	console.log(JSON.stringify(myObj));
+}
