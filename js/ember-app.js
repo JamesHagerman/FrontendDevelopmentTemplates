e6431606ce907da834e8eb4892830d18b02a6151
JamesHagerman <james.hagerman@gmail.com>
2013-04-18 15:46:21 -0700

    starting to add walkthrough instructions for building a new wireframe

--- js/ember-app.js
+++ js/ember-app.js
@@ -2,6 +2,8 @@ App = Ember.Application.create();
 
 App.Router.map(function() {
   // put your routes here
+  this.route("ember_intro", { path: "/ember_intro" });
+  this.route("skeleton_into", { path: "/skeleton_into" });
 });
 
 App.IndexRoute = Ember.Route.extend({
