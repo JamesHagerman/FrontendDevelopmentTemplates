14533130d8d8c55e4215f9020d843be2561090c8
JamesHagerman <james.hagerman@gmail.com>
2013-04-19 20:01:43 -0700

    adding some fixes and some styles to show some simple inline css to get things to play nicely in skeleton. Maybe this is going too far...

--- js/ember-app.js
+++ js/ember-app.js
@@ -2,11 +2,12 @@ App = Ember.Application.create();
 
 App.Router.map(function() {
   // put your routes here
-  this.route("ember_intro", { path: "/ember_intro" });
-  this.route("skeleton_into", { path: "/skeleton_into" });
+  this.route('ember_intro', { path: "/ember_intro" });
+  this.route('skeleton_into', { path: "/skeleton_into" });
 });
 
-App.IndexRoute = Ember.Route.extend({
+// Notice how the underscores in the route and path are removed and replaced with camelcase here:
+App.EmberIntroRoute = Ember.Route.extend({
   model: function() {
     return ['red', 'yellow', 'blue'];
   }
